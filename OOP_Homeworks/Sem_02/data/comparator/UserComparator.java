0dee390c0d1bdab3d1c05281b60fa249723cfc10
Ivan Balabay <i.balabai@gmail.com>
2023-01-10 18:52:36 +0300

    UserComparator class added

---
+++ OOP_Homeworks/Sem_02/data/comparator/UserComparator.java
@@ -0,0 +1,13 @@
+package Sem_02.data.comparator;
+
+import Sem_02.data.Student;
+
+import java.util.Comparator;
+
+public class UserComparator implements Comparator<Student> {
+
+    @Override
+    public int compare(Student student1, Student student2) {
+         return student1.getName().compareTo(student2.getName());
+    }
+}
