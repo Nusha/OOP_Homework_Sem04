9f0d95c10868160e98bd197bf474df3c8f2b6479
Ivan Balabay <i.balabai@gmail.com>
2023-01-10 19:30:03 +0300

    Stream sorting class and interface added

---
+++ OOP_Homeworks/Sem_02/service/GroupStreamServiceImpl.java
@@ -0,0 +1,16 @@
+package Sem_02.service;
+
+import Sem_02.data.GroupStream;
+import Sem_02.data.comparator.GroupStreamComparator;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class GroupStreamServiceImpl implements GroupStreamService{
+
+    @Override
+    public void sortStream(List groupStream) {
+        Collections.sort(groupStream, new GroupStreamComparator());
+    }
+}
