54c259c3fcc00f45eeaaa267e0315ad7fb13cc96
Ivan Balabay <i.balabai@gmail.com>
2023-01-12 19:25:00 +0300

    Invalid methods removed.

--- OOP_Homeworks/Sem_02/service/StudentServiceImpl.java
+++ OOP_Homeworks/Sem_02/service/StudentServiceImpl.java
@@ -1,13 +1,11 @@
 package Sem_02.service;
 
 import Sem_02.data.Student;
-import Sem_02.data.StudentGroup;
-import Sem_02.data.Teacher;
 import Sem_02.data.User;
 import Sem_02.util.ReaderFromTxt;
 import Sem_02.util.WriterToTxt;
 
-import java.util.List;
+
 
 public class StudentServiceImpl implements DataService {
 
@@ -22,29 +20,16 @@ public class StudentServiceImpl implements DataService {
 
     }
 
-    /** Работа над ошбками 2 урок.
-     * Чтобы реализовать этот метод возпользуетесь статическими методами из пекеджа utils для получения учителя и учеников
-     * Обратился к пэкэджу util, к статическим методам чтения учителя и листа учеников.
-     */
-    @Override
-    public StudentGroup createGroup(int groupNumber) {
-        Teacher teacher = ReaderFromTxt.readTeacherFromTxt();
-        List<Student> studentList= ReaderFromTxt.readStudentsFromTxt();
-        return new StudentGroup(teacher,studentList, groupNumber);
-    }
-
-
-
-    @Override
-    public StudentGroup getGroup() {
-        return null;
-    }
-
-    @Override
-    public StudentGroup getGroup(int number) {
-        return null;
-    }
-
+//    @Override
+//    public StudentGroup getGroup() {
+//        return null;
+//    }
+//
+//    @Override
+//    public StudentGroup getGroup(int number) {
+//        return null;
+//    }
+//
     @Override
     public void removeStudent(Student name) {
 
