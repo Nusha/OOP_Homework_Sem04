3192822427983a911ef6f4912da0fdf50f9ed4bc
Ivan Balabay <i.balabai@gmail.com>
2023-01-13 01:24:19 +0300

    Method compareName added. Orphaned import removed.

--- OOP_Homeworks/Sem_02/data/comparator/UserComparator.java
+++ OOP_Homeworks/Sem_02/data/comparator/UserComparator.java
@@ -1,13 +1,19 @@
 package Sem_02.data.comparator;
 
 import Sem_02.data.Student;
+import Sem_02.service.DataService;
 
 import java.util.Comparator;
 
-public class UserComparator implements Comparator<Student> {
+public class UserComparator implements  Comparator<Student> {
 
     @Override
     public int compare(Student student1, Student student2) {
          return student1.getName().compareTo(student2.getName());
     }
+
+    public int compareName(Student student1, Student student2) {
+        return (student1.getName().compareTo(student2.getName()));
+    }
+
 }
