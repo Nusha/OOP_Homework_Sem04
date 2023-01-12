8d389607706e78b55769592f50e4095028eca90c
Ivan Balabay <i.balabai@gmail.com>
2023-01-12 22:13:56 +0300

    Fixes.

--- OOP_Homeworks/Sem_02/service/GroupStreamServiceImpl.java
+++ OOP_Homeworks/Sem_02/service/GroupStreamServiceImpl.java
@@ -1,9 +1,7 @@
 package Sem_02.service;
 
-import Sem_02.data.GroupStream;
 import Sem_02.data.comparator.GroupStreamComparator;
 
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
@@ -12,5 +10,6 @@ public class GroupStreamServiceImpl implements GroupStreamService{
     @Override
     public void sortStream(List groupStream) {
         Collections.sort(groupStream, new GroupStreamComparator());
+
     }
 }
