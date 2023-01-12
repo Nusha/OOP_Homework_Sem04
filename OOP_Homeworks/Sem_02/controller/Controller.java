e704a08c67262865d08f5765ef351f8817e04ad3
Ivan Balabay <i.balabai@gmail.com>
2023-01-12 19:20:11 +0300

    Variables added, constructor for StudentGroupService added. Variables modified.

--- OOP_Homeworks/Sem_02/controller/Controller.java
+++ OOP_Homeworks/Sem_02/controller/Controller.java
@@ -1,23 +1,44 @@
 package Sem_02.controller;
 
-
 import Sem_02.data.Student;
 import Sem_02.data.StudentGroup;
-import Sem_02.service.DataService;
-import Sem_02.service.StudentServiceImpl;
+import Sem_02.service.*;
 
-public class Controller {
-    DataService studentService;
+
+public class Controller<G, I extends Number> {
+    private DataService studentService;
+    private StudentGroupService studentGroupService;
+    private GroupStreamService  groupStreamService;
 
     private Controller(StudentServiceImpl studentService) {
         this.studentService = studentService;
     }
+
+    public Controller(StudentGroupService studentGroupService) {
+        this.studentGroupService = studentGroupService;
+    }
+
     public Student createStudent(Student student) {
         studentService.create(student);
         return (Student) studentService.read(student);
     }
 
-    public StudentGroup createGroup(int groupnum) {
-    return (studentService.createGroup(groupnum));
+    public StudentGroup createGroup(int groupNumber) {
+        return studentGroupService.createGroup(groupNumber);
     }
+
+
+//    public Controller(GroupStreamServiceImpl streamService) {
+//        this.groupStreamService = streamService;
+//    }
+//
+//    public void streamListSort(List<GroupStream> groupStream)
+//    {
+//        groupStreamService.sortStream( groupStream);
+//    }
+
+
+//    public StudentGroup createGroup(int groupnum) {
+//    return (studentService.createGroup(groupnum));
+//    }
 }
