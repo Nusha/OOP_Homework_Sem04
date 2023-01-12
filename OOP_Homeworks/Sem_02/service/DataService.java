d3ed7815ff956cf78ac19e9b2def885594056651
Ivan Balabay <i.balabai@gmail.com>
2023-01-12 19:16:45 +0300

    DataService interface methods added, default methods removed.

--- OOP_Homeworks/Sem_02/service/DataService.java
+++ OOP_Homeworks/Sem_02/service/DataService.java
@@ -1,22 +1,14 @@
 package Sem_02.service;
 
 import Sem_02.data.Student;
-import Sem_02.data.StudentGroup;
 import Sem_02.data.User;
 
 public interface DataService {
-    default void create(User user){
+    void create(User user);
+    User read(User user); // Удалил дефолтные методы.
 
-    }
-    default User read(User user) {
+   
 
-        return user;
-    }
-    StudentGroup createGroup(int groupNumber);
-
-    StudentGroup getGroup();
-
-    StudentGroup getGroup(int number);
 
     void removeStudent (Student name);
 
